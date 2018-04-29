bb7b528084f177440db6fde81a60520b02ad9358
JaredAhaza <jaredahazq@gmail.com>
2018-04-29 10:35:02 +0300

    modified page

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,11 +1,11 @@
-// import { Component, OnInit } from '@angular/core';
-// import { ProfileService} from '../profile.service'
+import { Component, OnInit } from '@angular/core';
+import { ProfileService} from '../profile.service'
 
-// @Component({
-//   selector: 'app-profile',
-//   templateUrl: './profile.component.html',
-//   styleUrls: ['./profile.component.css']
-// })
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css']
+})
 export class ProfileComponent implements OnInit {
   app:any[];
   repos:any[];
