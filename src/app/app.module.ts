1d0873f7d13597d2c804a2e63fa2a85546ae9561
JaredAhaza <jaredahazq@gmail.com>
2018-04-30 07:39:01 +0300

    modified page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './profile/profile.component';
+import { StrikethroughDirective } from './strikethrough.directive';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     ProfileComponent,
+    StrikethroughDirective,
     
   ],
   imports: [
